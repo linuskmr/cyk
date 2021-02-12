f2f3613034bdb66b7cceb3ed5767632b768f75c0
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2021-02-12 09:23:16 +0100

    Initial commit

---
+++ cyk.js
@@ -0,0 +1,81 @@
+class CYK {
+    constructor(input) {
+        this.word = input.word
+        this.productions = input.productions
+
+        // Allocate 2d array for table of size word*word
+        this.table = []
+        for (let i = 0; i < this.word.length; i++) {
+            this.table[i] = []
+            for (let j = 0; j < this.word.length; j++) {
+                this.table[i][j] = null
+            }
+        }
+    }
+
+    get_producers(product) {
+        return new Set(this.productions
+            // Keep all productions which contain the searched product
+            .filter(production => production.products.has(product))
+            // Return producer (non-terminal)
+            .map(production => production.producer))
+    }
+
+    calc(start, end) {
+        // Cell is already calculated
+        if (this.table[start][end]) {
+            return this.table[start][end]
+        }
+
+        // Entry on the diagonal
+        if (start == end) {
+            this.table[start][end] = this.get_producers(this.word[start])
+            return this.table[start][end]
+        }
+
+        // Calculate cell
+        for (let i = start; i < end; i++) {
+            // Calculate word composition
+            const prefix = this.calc(start, i)
+            const suffix = this.calc(i+1, end)
+
+            if (!this.table[start][end]) {
+                this.table[start][end] = new Set()
+            }
+
+            // Try each permutation of prefix and suffix
+            for (let prefix_nt of prefix) {
+                for (let suffix_nt of suffix) {
+                    const producer = this.get_producers(prefix_nt + suffix_nt)
+
+                    // Merge old set with new entries
+                    this.table[start][end] = new Set([...this.table[start][end], ...producer])
+                }
+            }
+        }
+        return this.table[start][end]
+    }
+
+    toString() {
+        this.calc(0, this.word.length-1)
+
+        const PADDING = 7
+        let output = ''
+        for (let line of this.table) {
+            for (let item_set of line) {
+                let item_str = ''
+                if (!item_set) {
+                    // This field is not looked at
+                    item_str = '//'
+                } else if (item_set.size == 0) {
+                    item_str = '∅'
+                } else {
+                    item_str = Array.from(item_set).join()
+                }
+                output += item_str.padEnd(PADDING)
+            }
+            output += '\n'
+        }
+        return output
+    }
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+function parse_input() {
+    let productions_input = document.getElementById('productions_input').innerHTML
+    const word_input = document.getElementById('word_input').value.trim()
+
+    // Convert newlines to comma and remove spaces and dots
+    productions_input = productions_input.trim()
+    productions_input = productions_input.replace(/\n/g, ',')
+    productions_input = productions_input.replace(/ /g, '')
+    productions_input = productions_input.replace(/\./g, '')
+
+
+    // Replace html/latex arrows through simple - and >
+    productions_input = productions_input.replace(/→/g, '->')
+
+    // Replace latex short pipe ∣ with long keyboard pipe |
+    productions_input = productions_input.replace(/∣/g, '|')
+
+    const lines = productions_input.split(',')
+    let productions = []
+    for (let line of lines) {
+        // Left of -> is the producer and right are the products
+        let [producer, products] = line.split('->')
+        products = new Set(products.split('|'))
+        productions.push({producer, products})
+    }
+    const word = word_input
+    return {productions, word}
+}
+
+
+function render_cyk() {
+    const input = parse_input()
+    const cyk = new CYK(input)
+    const result = cyk.toString()
+    document.getElementById("output").innerHTML = result
+}
